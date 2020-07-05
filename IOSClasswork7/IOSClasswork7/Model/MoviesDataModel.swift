2abb349bee4681dd4b7442505fa91209c218483c
ljmutawa <laila.almutawa2004@gmail.com>
2020-07-05 03:33:25 +0300

    done!

--- IOSClasswork7/IOSClasswork7/Model/MoviesDataModel.swift
+++ IOSClasswork7/IOSClasswork7/Model/MoviesDataModel.swift
@@ -10,7 +10,7 @@ import Foundation
 
 
 
-class moviesModel {
+class MoviesModel {
     var movieName: String = ""
     var movieReleaseDate: Int
     var actors: [String]
@@ -32,13 +32,12 @@ class moviesModel {
 // املأ  المصففة  الموجودة  في ملف  MovieDataModel بكوائن من  أفلامك المفضلة
 //أو ازل الـComment عن المصفوقة التالية
 
-/*
-var MoviesData = [
-    moviesModel(name: "Avengers Endgame", release: 2019, actors: ["Robert Downey", "Chris Evans", "Chris Hemsworth"], rating: 8.4, rated: "PG-13"),
-    moviesModel(name: "Thor- Ragnarok", release: 2017, actors: ["Chris Hemsworth", "Tom Hiddleston", "Mark Ruffalo"], rating: 7.9, rated: "PG-13"),
-    moviesModel(name: "Captain America", release: 2011, actors: ["Chris Evans", "Hayley Atwell", "Sebastian Stan"], rating: 6.9, rated: "PG-13"),
-    moviesModel(name: "Black Panther", release: 2018, actors: ["Michael B. Jordan", "Lupita Nyong'o","Martin Freeman"], rating: 7.3, rated: "PG-13"),
-    moviesModel(name: "Guardians of the Galaxy", release: 2014, actors: ["Chris Pratt","Vin Diesel","Zoe Saldana"], rating: 8.0, rated: "PG-13"),
-    moviesModel(name: "Spider-Man", release: 2019, actors: ["Tom Holland", "Jake Gyllenhaal","Samuel L. Jackson"], rating: 7.5, rated: "PG-13")
+
+var moviesData = [
+    MoviesModel(name: "Avengers Endgame", release: 2019, actors: ["Robert Downey", "Chris Evans", "Chris Hemsworth"], rating: 8.4, rated: "PG-13"),
+    MoviesModel(name: "Thor- Ragnarok", release: 2017, actors: ["Chris Hemsworth", "Tom Hiddleston", "Mark Ruffalo"], rating: 7.9, rated: "PG-13"),
+    MoviesModel(name: "Captain America", release: 2011, actors: ["Chris Evans", "Hayley Atwell", "Sebastian Stan"], rating: 6.9, rated: "PG-13"),
+    MoviesModel(name: "Black Panther", release: 2018, actors: ["Michael B. Jordan", "Lupita Nyong'o","Martin Freeman"], rating: 7.3, rated: "PG-13"),
+    MoviesModel(name: "Guardians of the Galaxy", release: 2014, actors: ["Chris Pratt","Vin Diesel","Zoe Saldana"], rating: 8.0, rated: "PG-13"),
+    MoviesModel(name: "Spider-Man", release: 2019, actors: ["Tom Holland", "Jake Gyllenhaal","Samuel L. Jackson"], rating: 7.5, rated: "PG-13")
 ]
-*/
--- IOSClasswork7/IOSClasswork7/viewController/MovieDetailsViewController.swift
+++ IOSClasswork7/IOSClasswork7/viewController/MovieDetailsViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class MovieDetailsViewController: UIViewController {
     
     
-    var movieData : moviesModel = moviesModel(name: "Joker", release: 2000, actors: [""], rating: 10.0, rated: "PG-13")
+    var movieData : MoviesModel!
     
     @IBOutlet weak var movieImg: UIImageView!
     @IBOutlet weak var movieName: UINavigationItem!
@@ -51,13 +51,11 @@ class MovieDetailsViewController: UIViewController {
         
         self.actorImg1.image = UIImage(named: movieData.actors[0])
         self.actorName1.text = movieData.actors[0]
-        self.actorImg2.image = UIImage(named: movieData.actors[1])
+       self.actorImg2.image = UIImage(named: movieData.actors[1])
         self.actorName2.text = movieData.actors[1]
 
         self.actorImg3.image = UIImage(named: movieData.actors[2])
         self.actorName3.text = movieData.actors[2]
-
-        
     }
     
     func configureUI(){
